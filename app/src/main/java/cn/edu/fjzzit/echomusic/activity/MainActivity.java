00ee25d35edfdad2de4930cfdfe6080705b8f1bf
Mohsiaoran <moxiaoran5@gmail.com>
2021-05-20 20:00:20 +0800

    添加了登入按钮跳转新页面

--- app/src/main/java/cn/edu/fjzzit/echomusic/activity/MainActivity.java
+++ app/src/main/java/cn/edu/fjzzit/echomusic/activity/MainActivity.java
@@ -2,15 +2,35 @@ package cn.edu.fjzzit.echomusic.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import cn.edu.fjzzit.echomusic.R;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+
+    private Button loginBtn,regBtn,visitorBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+
+        loginBtn = findViewById(R.id.login_btn);
+        regBtn = findViewById(R.id.reg_btn);
+        visitorBtn = findViewById(R.id.visitor_btn);
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setClass(MainActivity.this, LoginActivity.class);
+                MainActivity.this.startActivity(intent);
+                finish();
+            }
+        });
     }
 }
