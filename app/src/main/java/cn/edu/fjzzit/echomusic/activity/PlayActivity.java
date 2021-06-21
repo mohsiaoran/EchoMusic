2eb519418096291fee6f09f01f4646a902db0eff
Mohsiaoran <moxiaoran5@gmail.com>
2021-06-21 09:24:19 +0800

    change

---
+++ app/src/main/java/cn/edu/fjzzit/echomusic/activity/PlayActivity.java
@@ -0,0 +1,17 @@
+package cn.edu.fjzzit.echomusic.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import cn.edu.fjzzit.echomusic.R;
+
+public class PlayActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_play);
+
+    }
+}
