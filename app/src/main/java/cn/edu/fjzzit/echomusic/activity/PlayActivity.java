507c6282772094c5eea090c5bfa207f30b824cd4
Mohsiaoran <moxiaoran5@gmail.com>
2021-06-21 23:14:13 +0800

    解决一些已知问题，添加进度条，播放bar透明处理

--- app/src/main/java/cn/edu/fjzzit/echomusic/activity/PlayActivity.java
+++ app/src/main/java/cn/edu/fjzzit/echomusic/activity/PlayActivity.java
@@ -3,16 +3,20 @@ package cn.edu.fjzzit.echomusic.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.SeekBar;
 
 import cn.edu.fjzzit.echomusic.R;
 
 public class PlayActivity extends AppCompatActivity {
 
     ImageButton returnBtn;
+    SeekBar seekBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +24,9 @@ public class PlayActivity extends AppCompatActivity {
         setContentView(R.layout.activity_play);
 
         returnBtn = findViewById(R.id.play_page_return_btn);
+        seekBar = findViewById(R.id.play_page_seekBar);
+
+        seekBar.getThumb().setColorFilter(Color.LTGRAY, PorterDuff.Mode.SRC_ATOP);
 
         returnBtn.setOnClickListener(new View.OnClickListener() {
             @Override
