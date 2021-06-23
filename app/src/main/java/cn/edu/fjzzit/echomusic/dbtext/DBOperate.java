f58622de18d8273f9833585dc17335c2647bfea1
wqy <2535569958@qq.com>
2021-06-23 23:23:05 +0800

    sqllite的删减

--- app/src/main/java/cn/edu/fjzzit/echomusic/dbtext/DBOperate.java
+++ app/src/main/java/cn/edu/fjzzit/echomusic/dbtext/DBOperate.java
@@ -11,14 +11,13 @@ public class DBOperate {
     public static cn.edu.fjzzit.echomusic.dbtext.DBOperate dbOperate = null;
     private DBHelper helper;
     private SQLiteDatabase sb;
-    private final String TABLE_NAME = "musicInfo";
 
     public DBOperate(Context context) {
         helper = new DBHelper(context, "musicInfo.db", null, 1);
         sb = helper.getReadableDatabase();
-        insertData(1,"canon", "Johann Pachelbel","canon");
-        insertData(2, "nocturne","Fryderyk","nocturne");
-        insertData(3, "alla turca","Mozart","alla turca");
+//        insertData(1,"canon", "Johann Pachelbel","canon");
+//        insertData(2, "nocturne","Fryderyk","nocturne");
+//        insertData(3, "alla turca","Mozart","alla turca");
     }
 
     //获取数据库操作实例
@@ -27,16 +26,6 @@ public class DBOperate {
             dbOperate = new cn.edu.fjzzit.echomusic.dbtext.DBOperate(context);
         }
         return dbOperate;
-    }
-
-    //读取数据库数据
-    public void readData(List<String> list) {
-        Cursor cursor = sb.query(TABLE_NAME, null, null, null, null, null, null);
-        while (cursor.moveToNext()) {
-            String musicInfoTitle = cursor.getString(cursor.getColumnIndex("title"));
-            list.add(musicInfoTitle);
-        }
-        cursor.close();
     }
 
     //根据传入的条件查询
@@ -51,9 +40,9 @@ public class DBOperate {
         cursor.close();
     }
 
-    //插入数据
-    public void insertData(Integer musicId, String title, String auther,String resUrl) {
-        sb.execSQL("insert INTO musicinfo(musicId,title,auther,resUrl) select ?,?,?,? WHERE NOT EXISTS(SELECT musicId FROM musicinfo WHERE musicId = 1 or 2 or 3)", new Object[]{musicId,title,auther,resUrl});
-    }
+//    //插入数据
+//    public void insertData(Integer musicId, String title, String auther,String resUrl) {
+//        sb.execSQL("insert INTO musicinfo(musicId,title,auther,resUrl) select ?,?,?,? WHERE NOT EXISTS(SELECT musicId FROM musicinfo WHERE musicId = 1 or 2 or 3)", new Object[]{musicId,title,auther,resUrl});
+//    }
 
 }
