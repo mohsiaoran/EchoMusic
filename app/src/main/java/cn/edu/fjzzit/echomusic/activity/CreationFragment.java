9c70ffcb94c2d26eaa3bf6ee90f24b4e866a64f9
林修 <1830621979@qq.com>
2021-07-02 15:26:28 +0800

    教程子页面

--- app/src/main/java/cn/edu/fjzzit/echomusic/activity/CreationFragment.java
+++ app/src/main/java/cn/edu/fjzzit/echomusic/activity/CreationFragment.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -25,6 +26,7 @@ import cn.edu.fjzzit.echomusic.entity.ToturialInfo;
 public class CreationFragment extends Fragment {
 
     private RecyclerView mToturialRecyclerView;
+    private ImageView toturialImage;//教程图标
     private TextView toturialMore; //精选更多
     private TextView actMore; //精选更多
 
@@ -34,6 +36,7 @@ public class CreationFragment extends Fragment {
         mToturialRecyclerView = view.findViewById(R.id.toturial_rlv);
         toturialMore =view.findViewById(R.id.tutorial_more_tv);
         actMore =view.findViewById(R.id.activity_more_tv);
+        toturialImage =view.findViewById(R.id.toturial_iv);
 
         List<ToturialInfo> toturialInfoList = new ArrayList<ToturialInfo>();
 
@@ -47,6 +50,18 @@ public class CreationFragment extends Fragment {
         ToturialAdapter toturialAdapter = new ToturialAdapter(toturialInfoList, view.getContext());
         mToturialRecyclerView.setAdapter(toturialAdapter);
 
+
+        //跳转到教程详情
+        toturialImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                Intent intent = new Intent();
+                intent.setClass(getContext(),ToturialActivity.class);
+                getContext().startActivity(intent);
+
+            }
+        });
 
         //跳转到推荐教程更多
         toturialMore.setOnClickListener(new View.OnClickListener() {
